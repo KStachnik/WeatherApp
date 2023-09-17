f15415d89ed6bd0bd69d5f1707e296fdc249b984
KStachnik <stachnikrystian@gmail.com>
2023-09-17 13:23:31 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Krystian Stachnik on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
